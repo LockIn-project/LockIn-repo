3b9b69cf31177ad5458184eee002d29d944bd0b6
Aaron Arada <aaronarada011@gmail.com>
2026-05-16 20:06:44 +0800

    Fixed Server Issues

--- backend/src/config/supabase.ts
+++ backend/src/config/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import ws from "ws";
 
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
+  process.env.SUPABASE_ANON_KEY!,
+  { global: { fetch }, realtime: { transport: ws as any } }
 );
